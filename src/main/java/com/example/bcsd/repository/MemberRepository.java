1fb89a5bdb4363ef741fc4dcc62f7f5e7348e4b7
JanooGwan <janoogwan39@gmail.com>
2025-06-23 21:47:37 +0900

    update : MemberRepository - JpaRepository를 상속한 형태로 변경

--- src/main/java/com/example/bcsd/repository/MemberRepository.java
+++ src/main/java/com/example/bcsd/repository/MemberRepository.java
@@ -1,16 +1,11 @@
 package com.example.bcsd.repository;
 
-
 import com.example.bcsd.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository {
-    List<Member> findAll();
-    Optional<Member> findById(Long id);
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
     Optional<Member> findByEmail(String email);
-    Member save(Member member);
-    void update(Member member, Long id);
-    boolean deleteById(Long id);
 }
