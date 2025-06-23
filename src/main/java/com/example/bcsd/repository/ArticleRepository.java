88f2af037b2fff42ebd38782956919efce8cbe74
JanooGwan <janoogwan39@gmail.com>
2025-06-23 21:45:41 +0900

    update : ArticleRepoisotry - JpaRepository를 상속한 형태로 변경, Impl 클래스들 삭제

--- src/main/java/com/example/bcsd/repository/ArticleRepository.java
+++ src/main/java/com/example/bcsd/repository/ArticleRepository.java
@@ -2,17 +2,13 @@ package com.example.bcsd.repository;
 
 
 import com.example.bcsd.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ArticleRepository {
-    List<Article> findAll();
+public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByBoardId(Long boardId);
-    Optional<Article> findById(Long id);
-    boolean existsByWriterId(Long id);
-    boolean existsByBoardId(Long id);
-    Article save(Article article);
-    void update(Article article);
-    boolean deleteById(Long id);
+    boolean existsByWriter_Id(Long id);
+    boolean existsByBoard_Id(Long id);
 }
