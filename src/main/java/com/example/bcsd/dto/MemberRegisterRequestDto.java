f96b039614c1694d378cd604e714ff7ce217dc8b
JanooGwan <janoogwan39@gmail.com>
2025-06-30 15:22:14 +0900

    feat : MemberRegisterRequestDto 작성

--- src/main/java/com/example/bcsd/dto/MemberRegisterRequestDto.java
+++ src/main/java/com/example/bcsd/dto/MemberRegisterRequestDto.java
@@ -1,4 +1,16 @@
 package com.example.bcsd.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class MemberRegisterRequestDto {
+    private String name;
+    private String email;
+    private String password;
 }
