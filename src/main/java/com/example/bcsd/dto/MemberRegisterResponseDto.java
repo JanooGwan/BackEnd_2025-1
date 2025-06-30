f733dc7d8606d796ebb73929f8ff028cc5a5dafa
JanooGwan <janoogwan39@gmail.com>
2025-06-30 15:29:46 +0900

    feat : MemberRegisterResponseDto 작성

--- src/main/java/com/example/bcsd/dto/MemberRegisterResponseDto.java
+++ src/main/java/com/example/bcsd/dto/MemberRegisterResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.bcsd.dto;
 
+import com.example.bcsd.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,11 @@ public class MemberRegisterResponseDto {
     private String name;
     private String email;
 
-
+    public static MemberRegisterResponseDto from(Member member) {
+        return new MemberRegisterResponseDto(
+                member.getId(),
+                member.getName(),
+                member.getEmail()
+        );
+    }
 }
