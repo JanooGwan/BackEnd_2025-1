52a6c3b00b8378402b2bcec00d7147b0e5044397
JanooGwan <janoogwan39@gmail.com>
2025-11-10 10:09:07 +0900

    feat: Article(Domain) 작성

---
+++ src/main/java/com/example/bcsd/domain/Article.java
@@ -0,0 +1,49 @@
+package com.example.bcsd.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+
+    public static Long articleCount = 0L;
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+
+    public Article(String title, String content) {
+        this.id = ++articleCount;
+        this.title = title;
+        this.content = content;
+        this.createdDate = LocalDateTime.now();
+        this.modifiedDate = null;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.modifiedDate = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+}
