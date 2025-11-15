6acfc5edf544bf8c9c7df4b432380f29eae9fed5
JanooGwan <janoogwan39@gmail.com>
2025-11-15 15:02:05 +0900

    feat: MemberResponse 작성

---
+++ src/main/java/com/example/bcsd/controller/dto/response/MemberResponse.java
@@ -0,0 +1,19 @@
+package com.example.bcsd.controller.dto.response;
+
+import com.example.bcsd.model.Member;
+
+public record MemberResponse(
+        Long id,
+        String name,
+        String email,
+        String password
+) {
+    public static MemberResponse from(Member member) {
+        return new MemberResponse(
+                member.getId(),
+                member.getName(),
+                member.getEmail(),
+                member.getPassword()
+        );
+    }
+}
