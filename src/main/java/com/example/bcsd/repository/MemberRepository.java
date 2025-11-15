4a726bbf3f7e2d5dd0d1b9958ce14e9ae120443f
JanooGwan <janoogwan39@gmail.com>
2025-11-15 15:35:36 +0900

    refactor: MemberRepository - findAll() 추가

--- src/main/java/com/example/bcsd/repository/MemberRepository.java
+++ src/main/java/com/example/bcsd/repository/MemberRepository.java
@@ -1,17 +1,23 @@
 package com.example.bcsd.repository;
 
+import com.example.bcsd.model.Article;
 import com.example.bcsd.model.Member;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class MemberRepository {
     private final AtomicLong key = new AtomicLong(0);
     private final Map<Long, Member> members = new HashMap<>();
+
+    public List<Member> findAll() {
+        List<Member> result = new ArrayList<>();
+        members.forEach((id, member) -> result.add(member));
+
+        return result;
+    }
 
     public Optional<Member> findById(Long id) {
         Member member = members.get(id);
