f978a757eeeaffbae2b8d4c7beaacab4962586b5
JanooGwan <janoogwan39@gmail.com>
2025-11-15 15:27:10 +0900

    feat: BoardService 작성

---
+++ src/main/java/com/example/bcsd/service/BoardService.java
@@ -0,0 +1,50 @@
+package com.example.bcsd.service;
+
+import com.example.bcsd.controller.dto.request.BoardCreateRequest;
+import com.example.bcsd.controller.dto.request.BoardUpdateRequest;
+import com.example.bcsd.controller.dto.response.BoardResponse;
+import com.example.bcsd.model.Board;
+import com.example.bcsd.repository.BoardRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public BoardResponse getBoardById(Long id) {
+        return boardRepository.findById(id)
+                .map(BoardResponse::from)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "ID에 해당하는 게시판이 존재하지 않습니다."));
+    }
+
+    public BoardResponse createBoard(BoardCreateRequest requestDto) {
+        Board board = boardRepository.save(requestDto.toEntity());
+        return BoardResponse.from(board);
+    }
+
+    public BoardResponse updateBoard(Long id, BoardUpdateRequest requestDto) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "ID에 해당하는 게시판이 존재하지 않습니다.")
+                );
+
+        board.update(requestDto.name(), requestDto.email(), requestDto.password());
+        Board updatedBoard = BoardRepository.update(id, board);
+
+        return BoardResponse.from(updatedBoard);
+    }
+
+
+    public void deleteBoard(Long id) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(HttpStatus.NOT_FOUND, "ID에 해당하는 게시판이 존재하지 않습니다.")
+                );
+
+        boardRepository.deleteById(id);
+    }
+}
