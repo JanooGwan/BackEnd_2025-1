ad575bb740accbd4c7f98b6ddf3d452a06d56012
JanooGwan <janoogwan39@gmail.com>
2025-12-22 15:12:01 +0900

    feat: Member Entity 작성

---
+++ src/main/java/com/example/bcsd/entity/Member.java
@@ -0,0 +1,36 @@
+package com.example.bcsd.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "member")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void update(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
