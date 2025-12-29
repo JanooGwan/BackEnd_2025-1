cfa12def5953f5e8cebc53834acbb4fad94c0af0
JanooGwan <janoogwan39@gmail.com>
2025-12-29 17:25:22 +0900

    refactor: N+1 문제 개선을 위한 ArticeRepository 변경

--- src/main/java/com/example/bcsd/repository/ArticleRepository.java
+++ src/main/java/com/example/bcsd/repository/ArticleRepository.java
@@ -2,9 +2,27 @@ package com.example.bcsd.repository;
 
 import com.example.bcsd.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findAllByBoardId(Long boardId);
+
+    @Query("SELECT a FROM Article a " + "JOIN FETCH a.writer " + "JOIN FETCH a.board")
+    List<Article> findAllWithWriterAndBoard();
+
+    @Query("SELECT a FROM Article a " + "JOIN FETCH a.writer " + "WHERE a.board.id = :boardId")
+    List<Article> findAllByBoardIdWithWriter(@Param("boardId") Long boardId);
+
+    @Query("SELECT a FROM Article a " + "JOIN FETCH a.writer " + "JOIN FETCH a.board " + "WHERE a.board.id = :boardId")
+    List<Article> findAllByBoardIdWithWriterAndBoard(@Param("boardId") Long boardId);
+
+    @Query("SELECT a FROM Article a " + "JOIN FETCH a.writer " + "JOIN FETCH a.board " + "WHERE a.id = :id")
+    Optional<Article> findByIdWithWriterAndBoard(@Param("id") Long id);
+
+    @Query("SELECT a FROM Article a " + "JOIN FETCH a.writer")
+    List<Article> findAllWithWriter();
 }
