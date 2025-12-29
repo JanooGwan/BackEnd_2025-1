3b507dcddd6b6f0d3f6782e65356005e40a0cfca
JanooGwan <janoogwan39@gmail.com>
2025-12-29 20:03:28 +0900

    refactor: N+1 문제 개선을 위한 BoardRepository 변경

--- src/main/java/com/example/bcsd/repository/BoardRepository.java
+++ src/main/java/com/example/bcsd/repository/BoardRepository.java
@@ -5,7 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
     @Query("SELECT COUNT(a) FROM Article a WHERE a.board.id = :boardId")
     int countArticlesByBoard(@Param("boardId") Long boardId);
+
+    @Query("SELECT DISTINCT b FROM Board b " + "LEFT JOIN FETCH b.articles")
+    List<Board> findAllWithArticles();
+
+    @Query("SELECT DISTINCT b FROM Board b " + "LEFT JOIN FETCH b.articles a " + "LEFT JOIN FETCH a.writer")
+    List<Board> findAllWithArticlesAndWriter();
 }
